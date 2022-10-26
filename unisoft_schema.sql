6dcfd3ef7a456aa07ec3f1d9fce0154e68489e3a
Racknaro <racknarow1@gmail.com>
2022-10-25 21:06:52 -0500

    add schema

--- unisoft_schema.sql
+++ unisoft_schema.sql
@@ -1,36 +1,119 @@
-CREATE TABLE clientes (
-	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-	dni VARCHAR(15),
-	nombre VARCHAR(100),
-	apellido VARCHAR(100),
-	fecha_nacimiento DATE
-);
-	
-	
-CREATE TABLE venta (
-	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-	fecha_venta DATE,
-	total DOUBLE,
-	id_cliente INT,
-	CONSTRAINT fk_id_cliente FOREIGN KEY (id_cliente) REFERENCES clientes(id)
-);
+/*
+ Navicat Premium Data Transfer
 
-CREATE TABLE productos(
-	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-	sku VARCHAR(50),
-	descripcion VARCHAR(150),
-	precio_compra DOUBLE,
-	precio_venta DOUBLE,
-	stock INT,
-	ultima_compra DATE
-);
-	
-CREATE TABLE detalle_venta(
-	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-	cantidad INT,
-	subtotal DOUBLE,
-	id_venta INT,
-	id_producto INT,
-	CONSTRAINT fk_id_producto FOREIGN KEY (id_producto) REFERENCES productos(id),
-	CONSTRAINT fk_id_venta FOREIGN KEY (id_venta) REFERENCES venta(id)
-);
+ Source Server         : Localhost
+ Source Server Type    : MySQL
+ Source Server Version : 80030
+ Source Host           : localhost:3306
+ Source Schema         : coder_unisoft_store
+
+ Target Server Type    : MySQL
+ Target Server Version : 80030
+ File Encoding         : 65001
+
+ Date: 25/10/2022 21:04:47
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for clientes
+-- ----------------------------
+DROP TABLE IF EXISTS `clientes`;
+CREATE TABLE `clientes`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `dni` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `nombre` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `apellido` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `fecha_nacimiento` date NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of clientes
+-- ----------------------------
+INSERT INTO `clientes` VALUES (1, '1057587963', 'Pedro', 'Juarez', '1991-09-01');
+INSERT INTO `clientes` VALUES (2, '1057587963', 'Cristian', 'Alvarez', '1992-09-01');
+INSERT INTO `clientes` VALUES (3, '1057587963', 'Juanito', 'Torres', '1991-09-01');
+INSERT INTO `clientes` VALUES (4, '1057587963', 'Chapulin', 'Colorado', '1992-09-01');
+
+-- ----------------------------
+-- Table structure for detalle_venta
+-- ----------------------------
+DROP TABLE IF EXISTS `detalle_venta`;
+CREATE TABLE `detalle_venta`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `cantidad` int NULL DEFAULT NULL,
+  `subtotal` double NULL DEFAULT NULL,
+  `id_venta` int NULL DEFAULT NULL,
+  `id_producto` int NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `fk_id_producto`(`id_producto` ASC) USING BTREE,
+  INDEX `fk_id_venta`(`id_venta` ASC) USING BTREE,
+  CONSTRAINT `fk_id_producto` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `fk_id_venta` FOREIGN KEY (`id_venta`) REFERENCES `venta` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of detalle_venta
+-- ----------------------------
+INSERT INTO `detalle_venta` VALUES (1, 4, 400, 7, 1);
+INSERT INTO `detalle_venta` VALUES (2, 3, 300, 8, 2);
+
+-- ----------------------------
+-- Table structure for hibernate_sequence
+-- ----------------------------
+DROP TABLE IF EXISTS `hibernate_sequence`;
+CREATE TABLE `hibernate_sequence`  (
+  `next_val` bigint NULL DEFAULT NULL
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of hibernate_sequence
+-- ----------------------------
+INSERT INTO `hibernate_sequence` VALUES (9);
+
+-- ----------------------------
+-- Table structure for productos
+-- ----------------------------
+DROP TABLE IF EXISTS `productos`;
+CREATE TABLE `productos`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `sku` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `descripcion` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `precio_compra` double NULL DEFAULT NULL,
+  `precio_venta` double NULL DEFAULT NULL,
+  `stock` int NULL DEFAULT NULL,
+  `ultima_compra` date NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of productos
+-- ----------------------------
+INSERT INTO `productos` VALUES (1, 'aaa111222a', 'Silla gamer', 2000, 2500, 10, '2022-10-25');
+INSERT INTO `productos` VALUES (2, 'aaa111222a', 'Puerta gamer', 2000, 2500, 10, '2022-10-25');
+
+-- ----------------------------
+-- Table structure for venta
+-- ----------------------------
+DROP TABLE IF EXISTS `venta`;
+CREATE TABLE `venta`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `fecha_venta` date NULL DEFAULT NULL,
+  `total` double NULL DEFAULT NULL,
+  `id_cliente` int NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `fk_id_cliente`(`id_cliente` ASC) USING BTREE,
+  CONSTRAINT `fk_id_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of venta
+-- ----------------------------
+INSERT INTO `venta` VALUES (6, '2022-10-11', 50, 1);
+INSERT INTO `venta` VALUES (7, '2022-10-11', 50, 2);
+INSERT INTO `venta` VALUES (8, '2022-10-11', 50, 1);
+
+SET FOREIGN_KEY_CHECKS = 1;
